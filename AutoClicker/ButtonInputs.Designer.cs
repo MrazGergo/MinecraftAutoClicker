57926d628523261bf6bca5c522dbc4fa65a0992d
Gerezd <36887090+MrazGergo@users.noreply.github.com>
2019-12-14 18:23:37 +0100

    ButtonInputs display bug fixed.
    Added an application icon.

--- AutoClicker/ButtonInputs.Designer.cs
+++ AutoClicker/ButtonInputs.Designer.cs
@@ -37,12 +37,12 @@
             // 
             // cbButtonEnable
             // 
-            this.cbButtonEnable.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.cbButtonEnable.AutoSize = true;
             this.cbButtonEnable.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.cbButtonEnable.Location = new System.Drawing.Point(18, 12);
+            this.cbButtonEnable.Location = new System.Drawing.Point(24, 15);
+            this.cbButtonEnable.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cbButtonEnable.Name = "cbButtonEnable";
-            this.cbButtonEnable.Size = new System.Drawing.Size(107, 21);
+            this.cbButtonEnable.Size = new System.Drawing.Size(126, 24);
             this.cbButtonEnable.TabIndex = 1;
             this.cbButtonEnable.Text = "Button name";
             this.cbButtonEnable.UseVisualStyleBackColor = true;
@@ -50,13 +50,13 @@
             // 
             // cbHold
             // 
-            this.cbHold.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.cbHold.AutoSize = true;
             this.cbHold.Enabled = false;
             this.cbHold.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.cbHold.Location = new System.Drawing.Point(18, 48);
+            this.cbHold.Location = new System.Drawing.Point(24, 59);
+            this.cbHold.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cbHold.Name = "cbHold";
-            this.cbHold.Size = new System.Drawing.Size(100, 21);
+            this.cbHold.Size = new System.Drawing.Size(117, 24);
             this.cbHold.TabIndex = 2;
             this.cbHold.Text = "Hold button";
             this.cbHold.UseVisualStyleBackColor = true;
@@ -64,28 +64,28 @@
             // 
             // lblDelay
             // 
-            this.lblDelay.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.lblDelay.AutoSize = true;
             this.lblDelay.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.lblDelay.Location = new System.Drawing.Point(19, 83);
+            this.lblDelay.Location = new System.Drawing.Point(20, 104);
+            this.lblDelay.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblDelay.Name = "lblDelay";
-            this.lblDelay.Size = new System.Drawing.Size(80, 17);
+            this.lblDelay.Size = new System.Drawing.Size(97, 20);
             this.lblDelay.TabIndex = 3;
             this.lblDelay.Text = "Delay (ms):";
             // 
             // numDelay
             // 
-            this.numDelay.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.numDelay.Enabled = false;
             this.numDelay.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.numDelay.Location = new System.Drawing.Point(105, 83);
+            this.numDelay.Location = new System.Drawing.Point(140, 102);
+            this.numDelay.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.numDelay.Minimum = new decimal(new int[] {
             1,
             0,
             0,
             0});
             this.numDelay.Name = "numDelay";
-            this.numDelay.Size = new System.Drawing.Size(120, 23);
+            this.numDelay.Size = new System.Drawing.Size(160, 26);
             this.numDelay.TabIndex = 4;
             this.numDelay.Value = new decimal(new int[] {
             1,
@@ -93,16 +93,18 @@
             0,
             0});
             // 
-            // UserInput
+            // ButtonInputs
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
             this.Controls.Add(this.numDelay);
             this.Controls.Add(this.lblDelay);
             this.Controls.Add(this.cbHold);
             this.Controls.Add(this.cbButtonEnable);
-            this.Name = "UserInput";
-            this.Size = new System.Drawing.Size(240, 130);
+            this.Margin = new System.Windows.Forms.Padding(0);
+            this.Name = "ButtonInputs";
+            this.Size = new System.Drawing.Size(320, 160);
             ((System.ComponentModel.ISupportInitialize)(this.numDelay)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
--- AutoClicker/Main.Designer.cs
+++ AutoClicker/Main.Designer.cs
@@ -28,19 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
             this.btn_start = new System.Windows.Forms.Button();
             this.lblStartTime = new System.Windows.Forms.Label();
             this.btn_stop = new System.Windows.Forms.Button();
-            this.biLeftMouse = new AutoClicker.ButtonInputs();
-            this.biRightMouse = new AutoClicker.ButtonInputs();
             this.lblStarted = new System.Windows.Forms.Label();
+            this.biRightMouse = new AutoClicker.ButtonInputs();
+            this.biLeftMouse = new AutoClicker.ButtonInputs();
             this.SuspendLayout();
             // 
             // btn_start
             // 
-            this.btn_start.Location = new System.Drawing.Point(269, 160);
+            this.btn_start.Location = new System.Drawing.Point(359, 197);
+            this.btn_start.Margin = new System.Windows.Forms.Padding(4);
             this.btn_start.Name = "btn_start";
-            this.btn_start.Size = new System.Drawing.Size(88, 51);
+            this.btn_start.Size = new System.Drawing.Size(117, 63);
             this.btn_start.TabIndex = 0;
             this.btn_start.Text = "START!";
             this.btn_start.UseVisualStyleBackColor = true;
@@ -50,9 +52,10 @@
             // 
             this.lblStartTime.AutoSize = true;
             this.lblStartTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.lblStartTime.Location = new System.Drawing.Point(233, 222);
+            this.lblStartTime.Location = new System.Drawing.Point(311, 273);
+            this.lblStartTime.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblStartTime.Name = "lblStartTime";
-            this.lblStartTime.Size = new System.Drawing.Size(34, 17);
+            this.lblStartTime.Size = new System.Drawing.Size(41, 20);
             this.lblStartTime.TabIndex = 2;
             this.lblStartTime.Text = "time";
             this.lblStartTime.Visible = false;
@@ -60,44 +63,50 @@
             // btn_stop
             // 
             this.btn_stop.Enabled = false;
-            this.btn_stop.Location = new System.Drawing.Point(165, 160);
+            this.btn_stop.Location = new System.Drawing.Point(220, 197);
+            this.btn_stop.Margin = new System.Windows.Forms.Padding(4);
             this.btn_stop.Name = "btn_stop";
-            this.btn_stop.Size = new System.Drawing.Size(88, 51);
+            this.btn_stop.Size = new System.Drawing.Size(117, 63);
             this.btn_stop.TabIndex = 3;
             this.btn_stop.Text = "STOP!";
             this.btn_stop.UseVisualStyleBackColor = true;
             this.btn_stop.Click += new System.EventHandler(this.Btn_stop_Click);
             // 
-            // biLeftMouse
-            // 
-            this.biLeftMouse.Location = new System.Drawing.Point(12, 18);
-            this.biLeftMouse.Name = "biLeftMouse";
-            this.biLeftMouse.Size = new System.Drawing.Size(240, 130);
-            this.biLeftMouse.TabIndex = 4;
-            // 
-            // biRightMouse
-            // 
-            this.biRightMouse.Location = new System.Drawing.Point(258, 18);
-            this.biRightMouse.Name = "biRightMouse";
-            this.biRightMouse.Size = new System.Drawing.Size(240, 130);
-            this.biRightMouse.TabIndex = 5;
-            // 
             // lblStarted
             // 
             this.lblStarted.AutoSize = true;
             this.lblStarted.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.lblStarted.Location = new System.Drawing.Point(162, 222);
+            this.lblStarted.Location = new System.Drawing.Point(216, 273);
+            this.lblStarted.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblStarted.Name = "lblStarted";
-            this.lblStarted.Size = new System.Drawing.Size(74, 17);
+            this.lblStarted.Size = new System.Drawing.Size(87, 20);
             this.lblStarted.TabIndex = 6;
             this.lblStarted.Text = "Started at:";
             this.lblStarted.Visible = false;
             // 
+            // biRightMouse
+            // 
+            this.biRightMouse.AutoSize = true;
+            this.biRightMouse.Location = new System.Drawing.Point(359, 22);
+            this.biRightMouse.Margin = new System.Windows.Forms.Padding(5);
+            this.biRightMouse.Name = "biRightMouse";
+            this.biRightMouse.Size = new System.Drawing.Size(320, 160);
+            this.biRightMouse.TabIndex = 5;
+            // 
+            // biLeftMouse
+            // 
+            this.biLeftMouse.AutoSize = true;
+            this.biLeftMouse.Location = new System.Drawing.Point(14, 22);
+            this.biLeftMouse.Margin = new System.Windows.Forms.Padding(0);
+            this.biLeftMouse.Name = "biLeftMouse";
+            this.biLeftMouse.Size = new System.Drawing.Size(320, 160);
+            this.biLeftMouse.TabIndex = 4;
+            // 
             // Main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(518, 250);
+            this.ClientSize = new System.Drawing.Size(691, 308);
             this.Controls.Add(this.lblStarted);
             this.Controls.Add(this.biRightMouse);
             this.Controls.Add(this.biLeftMouse);
@@ -105,6 +114,8 @@
             this.Controls.Add(this.lblStartTime);
             this.Controls.Add(this.btn_start);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Main";
@@ -119,9 +130,9 @@
         private System.Windows.Forms.Button btn_start;
         private System.Windows.Forms.Label lblStartTime;
         private System.Windows.Forms.Button btn_stop;
-        private ButtonInputs biLeftMouse;
-        private ButtonInputs biRightMouse;
         private System.Windows.Forms.Label lblStarted;
+        private ButtonInputs biRightMouse;
+        private ButtonInputs biLeftMouse;
     }
 }
 
